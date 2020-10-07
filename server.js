5e1320906cc9ef18836c7bf10c57c97e1f7d8698
Mitharu <46781785+Mitharu@users.noreply.github.com>
2020-10-08 00:47:53 +0700

    Update Percobaan Chat
    Perubahan server.js dan index.html disesuaikan dari angga

--- server.js
+++ server.js
@@ -12,9 +12,22 @@ const server = express()
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
+/*io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
-});
+});*/
+
+io.on('connect',function(socket){
+    console.log("Ada user yang konek : "+socket.id);
+
+    socket.on('pesan',function(data){
+        var sockets = io.sockets.sockets;
+        socket.broadcast.emit('pesan',data);
+    })    
+
+    socket.on('disconnect',function(){
+        console.log("User : "+socket.id+" Keluar");
+    })
+})
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
